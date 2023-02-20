93da3c76456d53b5a704ddbd04ef285624ff04a7
ugurcan.caykara <ugurcan.caykara@kloia.com>
2023-02-20 16:47:42 +0300

    update

--- db/db.go
+++ db/db.go
@@ -110,13 +110,13 @@ func newDynamoDBRequest(db Database, entityParsed map[string]*dynamodb.Attribute
 	return req
 }
 
-func (db Database) UpdateMovie(movie Movie, ctx context.Context, sensor *instana.Sensor, parentSp opentracing.Span) (Movie, error) {
+func (db Database) UpdateMovie(movie Movie, ctx context.Context, sensor *instana.Sensor, openParentSp opentracing.Span) (Movie, error) {
 	entityParsed, err := dynamodbattribute.MarshalMap(movie)
-	//parentSp := sensor.Tracer().StartSpan("testing", opentracing.Tags{
-	//	"dynamodb.op":     "get",
-	//	"dynamodb.table":  "test-table",
-	//	"dynamodb.region": "mock-region",
-	//})
+	parentSp := sensor.Tracer().StartSpan("testing", opentracing.Tags{
+		"dynamodb.op":     "get",
+		"dynamodb.table":  "test-table",
+		"dynamodb.region": "mock-region",
+	})
 
 	if err != nil {
 		return Movie{}, err
@@ -128,20 +128,17 @@ func (db Database) UpdateMovie(movie Movie, ctx context.Context, sensor *instana
 	}
 	req := newDynamoDBRequest(db, entityParsed)
 	req.SetContext(instana.ContextWithSpan(req.Context(), parentSp))
-	//_, err = db.client.PutItem(input)
-	//var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
-	//defer cancel()
-	//instaawssdk.StartDynamoDBSpan(req, sensor)
+	instaawssdk.StartDynamoDBSpan(req, sensor)
 	//fmt.Println(req)
-	//sp, _ := instana.SpanFromContext(req.Context())
+	sp, _ := instana.SpanFromContext(req.Context())
 
 	_, err = db.client.PutItemWithContext(req.Context(), input)
 	if err != nil {
 		return Movie{}, err
 	}
-	//sp.Finish()
-	//defer parentSp.Finish()
-	//instaawssdk.FinalizeDynamoDBSpan(req)
+	defer sp.Finish()
+	defer parentSp.Finish()
+	instaawssdk.FinalizeDynamoDBSpan(req)
 	return movie, nil
 }
 
